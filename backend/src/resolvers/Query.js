e7354842dd745961f669aa77320ac1f732f9b387
mughees605 <mughees605@gmail.com>
2019-02-03 15:26:39 +0500

    classesInIamAsMember query resolver completed

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -10,6 +10,22 @@ const Query = {
           }
         }, info)
       },
+     async classesInIamAsMember(parent, args, ctx, info){
+        //1 check if the user is loggedin or not
+        if (!ctx.request.userId) {
+          throw new Error("You must be logged in!")
+        }
+        //2 get all classes created by him and the classes in which he is as a member
+        const classes = await ctx.db.query.classes({
+          where : {
+            members_some : {
+              id_in : ctx.request.userId
+            },
+          },
+        }, info);
+        //3 return classes
+        return classes
+      },
 };
 
 module.exports = Query;
