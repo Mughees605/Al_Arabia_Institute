068f626c4f8cb365be0f578c0302e7164e38d0f1
mughees605 <mughees605@gmail.com>
2019-02-20 17:21:14 +0500

    class query resolver

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,5 +1,7 @@
+const { forwardTo } = require("prisma-binding");
 
 const Query = {
+    class: forwardTo('db'),
     me(parent, args, ctx, info) {
         if (!ctx.request.userId) {
           return null;
